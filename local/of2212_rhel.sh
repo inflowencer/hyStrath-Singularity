de7bea9589aa8d04f87f1d3bf2312b71cc220b39
inflowencer <maximilian.maigler@unibw.de>
2023-03-07 16:02:07 +0100

    Added local install script

---
+++ local/of2212_rhel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+module load USER-SPACK
+spack load /lp3kap5 /mmysxq2 /oqgii5k /kc4wk4e /jcboq73 /k4dmgwz /eckjnbs /yiwia6b /awceoya /rcfm42w /ucmnoqh /isbgfdc /m4lmpc5 /kl5z5co
+spack load parmgridgen
+
+export CC=/beegfs/home/m/maiglerm/USER-SPACK/0.18.1/opt/spack/linux-rocky8-icelake/gcc-8.5.0/gcc-12.2.0-lp3kap5i7kc5523yujivxl3ajhr2wcrn/bin/gcc
+export CXX=/beegfs/home/m/maiglerm/USER-SPACK/0.18.1/opt/spack/linux-rocky8-icelake/gcc-8.5.0/gcc-12.2.0-lp3kap5i7kc5523yujivxl3ajhr2wcrn/bin/g++
